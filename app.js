27be4b9ba1d457cecf0bc6b70a3ce42593974615
ZeeTom <zthom13@hotmail.com>
2021-06-29 17:40:06 -0600

    part 1 and 2

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
-console.log("Let's get this party started!");
+"use strict";
+let userInput;
+
+$("form").on("submit", getSearch);
+const API_KEY = "ePFJzVv31YN2Pe8nhzaBJeJ6GqfNOYZb";
+async function getSearch(e) {
+  e.preventDefault();
+  userInput = $("input").val();
+  let imgURL = await getGiphy();
+  appendToDOM(imgURL);
+}
+
+async function getGiphy() {
+  let res = await axios.get(
+    `http://api.giphy.com/v1/gifs/search?q=${userInput}&api_key=${API_KEY}`,
+    { headers: { accept: "application/json" } }
+  );
+  let imgURL = res.data.data[0].images.original.url;
+  console.log(imgURL);
+  return imgURL;
+}
+
+function appendToDOM(imgURL) {
+  let newImg = $(`<div><img src='${imgURL}'></div>`);
+  $("#img-container").append(newImg);
+}
