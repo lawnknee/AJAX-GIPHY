ee41ff3ab766ed56afa442225ed17e191f389487
Loni <kuangloni@gmail.com>
2021-06-29 17:10:40 -0700

    remove button works

--- app.js
+++ app.js
@@ -1,20 +1,24 @@
 "use strict";
 let userInput;
+const API_KEY = "ePFJzVv31YN2Pe8nhzaBJeJ6GqfNOYZb";
 
 $("form").on("submit", getSearch);
-const API_KEY = "ePFJzVv31YN2Pe8nhzaBJeJ6GqfNOYZb";
+
 async function getSearch(e) {
   e.preventDefault();
+
   userInput = $("input").val();
   let imgURL = await getGiphy();
   appendToDOM(imgURL);
+  $('form')[0].reset();
 }
 
 async function getGiphy() {
   let res = await axios.get(
-    `http://api.giphy.com/v1/gifs/search?q=${userInput}&api_key=${API_KEY}`,
-    { headers: { accept: "application/json" } }
+    `http://api.giphy.com/v1/gifs/search?q=${userInput}&api_key=${API_KEY}`
+    // { headers: { accept: "application/json" } }
   );
+  console.log(res)
   let imgURL = res.data.data[0].images.original.url;
   console.log(imgURL);
   return imgURL;
@@ -24,3 +28,8 @@ function appendToDOM(imgURL) {
   let newImg = $(`<div><img src='${imgURL}'></div>`);
   $("#img-container").append(newImg);
 }
+
+$('#remove').on("click", function(e){
+  e.preventDefault();
+  $("#img-container").empty();
+})
